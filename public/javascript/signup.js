b41fd39ddef0561048e58a43ddf73162d6e8f89d
Jeff Anderson-Lester <viperhq@hotmail.com>
2021-11-23 21:14:06 -0600

    created sign-up page, sign-up page now works.

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,13 +1,13 @@
 async function signupFormHandler(event) {
   event.preventDefault();
 
-  const username = document.querySelector("#user").value.trim();
+  const username = document.querySelector("#username").value.trim();
   const email = document.querySelector("#email").value.trim();
   const password = document.querySelector("#password").value.trim();
   const first_name = document.querySelector("#first_name").value.trim();
   const last_name = document.querySelector("#last_name").value.trim();
   const phone_number = document.querySelector("#phone_number").value.trim();
-  const title = document.querySelector("#title").value.trim();
+  const position_title = document.querySelector("#position_title").value.trim();
 
   if (
     username &&
@@ -16,7 +16,7 @@ async function signupFormHandler(event) {
     first_name &&
     last_name &&
     phone_number &&
-    title
+    position_title
   ) {
     const response = await fetch("/api/users", {
       method: "post",
@@ -34,9 +34,13 @@ async function signupFormHandler(event) {
 
     // check the response status
     if (response.ok) {
-      console.log("success");
+      document.location.replace("/");
     } else {
       alert(response.statusText);
     }
   }
 }
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
